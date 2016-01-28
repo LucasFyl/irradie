a02c95e97eea559f8d01cb9e7a77b42c2ca70151
Lucas Fayolle <lfayolle1@gmail.com>
2016-01-28 20:13:04 +0100

    adding in missing files

---
+++ app/assets/scripts/menu.js
@@ -0,0 +1,22 @@
+(function(){
+  'use strict';
+	var Menu = {
+		init: function(){
+        Menu.config = {
+            menu: $('#menu'),
+            trigger: $('.menu-trigger')
+        };
+
+        Menu.config.trigger.on('click', function(e){
+          e.preventDefault();
+          TweenMax.to('#menu', 1, {opacity:1,display:'block',ease:Expo.easeOut});
+        });
+    },
+    openMenu: function() {
+
+    },
+    closeMenu: function(){
+
+    }
+  };
+})();
