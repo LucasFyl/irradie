672a56463e2e0ab459cc4a84fa996e37f9f604f9
Lucas Fayolle <lfayolle1@gmail.com>
2016-01-31 17:36:47 +0100

    error page echoes message

--- app/site/templates/default.php
+++ app/site/templates/default.php
@@ -1,4 +1,9 @@
 <?php snippet('head') ?>
 <?php snippet('header') ?>
 
+  <div id="main">
+    <h1><?php echo $page->title() ?></h1>
+    <h2><?php echo $page->text() ?></h2>
+  </div>
+
 <?php snippet('footer') ?>
