0f23f2a2b79e56e41f446db14202fb363635d632
Lucas Fayolle <lfayolle1@gmail.com>
2016-02-05 11:48:28 +0100

    better prevnext layout

--- app/site/snippets/prev-next.php
+++ app/site/snippets/prev-next.php
@@ -1,36 +1,40 @@
 <?php if( $prev = $page->prevVisible() ):  ?>
 <div class="block-prevnext prev">
-	<div class="title-block">
-        <a href="<?php echo $page->prev()->url() ?>">
-          <p>
-            <?php echo $page->prev()->title() ?>
-            <span><?php echo $page->prev()->projectType2() ?></span>
-          </p>
-        </a>
-	</div>
+	<a href="<?php echo $prev->url() ?>">
+		<div class="title-block">
+	        <a href="<?php echo $prev->url() ?>">
+	          <p>
+	            <?php echo $prev->title() ?>
+	            <span><?php echo $prev->projectType2() ?></span>
+	          </p>
+	        </a>
+		</div>
 
-	<div class="img-wrap">
-		<?php foreach ($page->prev()->images()->slice(1, 3) as $image): ?>
-        <img src="<?php echo $image->url() ?>" alt="<?php echo $page->prev()->title() ?>" / draggable="false">
-		<?php endforeach; ?>
-	</div>
+		<div class="img-wrap">
+			<?php foreach ($prev->images()->slice(1, 3) as $image): ?>
+	        <img src="<?php echo $image->url() ?>" alt="<?php echo $prev->title() ?>" / draggable="false">
+			<?php endforeach; ?>
+		</div>
+	</a>
 </div>
 <?php endif ?>
 
 <?php if( $next = $page->nextVisible() ): ?>
 <div class="block-prevnext next">
-	<div class="title-block">
-        <a href="<?php echo $page->next()->url() ?>">
-          <p>
-            <?php echo $page->next()->title() ?>
-            <span><?php echo $page->next()->projectType1() ?></span>
-          </p>
-        </a>
-	</div>
-	<div class="img-wrap">
-		<?php foreach ($page->next()->images()->slice(1, 3) as $image): ?>
-        <img src="<?php echo $image->url() ?>" alt="<?php echo $page->next()->title() ?>" / draggable="false">
-		<?php endforeach; ?>
-	</div>
+	<a href="<?php echo $next->url() ?>">
+		<div class="title-block">
+	        <a href="<?php echo $next->url() ?>">
+	          <p>
+	            <?php echo $next->title() ?>
+	            <span><?php echo $next->projectType1() ?></span>
+	          </p>
+	        </a>
+		</div>
+		<div class="img-wrap">
+			<?php foreach ($next->images()->slice(1, 3) as $image): ?>
+	        <img src="<?php echo $image->url() ?>" alt="<?php echo $next->title() ?>" / draggable="false">
+			<?php endforeach; ?>
+		</div>
+	</a>
 </div>
 <?php endif ?>
