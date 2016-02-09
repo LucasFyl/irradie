5271b6204066a6c4b637d7505704f2b73c42578d
Lucas Fayolle <lfayolle1@gmail.com>
2016-02-09 19:25:38 +0100

    add previous page button support on dynamic nav

--- app/assets/scripts/dynamicNav.js
+++ app/assets/scripts/dynamicNav.js
@@ -3,6 +3,7 @@
 /* jshint latedef:false */
 (function(){
   'use strict';
+
 	function loadContent(_href) {
     TweenMax.to(window, 1, {scrollTo:0,ease:Power2.easeOut});
 		TweenMax.to('#loader', 0.5, {delay:0.5,opacity:1,visibility:'visible',onComplete:function(){
@@ -23,16 +24,18 @@
     	// load the content
     	loadContent(_href);
   	});
-    window.addEventListener('popstate', function(event) {
-      if (!everPushedSomething) {
-        /* jshint ignore:start */
-        var _href = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
-        /* jshint ignore:end */
-        loadContent(_href);
-      }
-        everPushedSomething = true;
-    });
+  } else {
+      // history is not supported; nothing fancy here
   }
+
+  // Bind popstate event to redirect when use hits 'previous page' button
+  $(window).bind("popstate", function() {
+    if (!everPushedSomething) {
+      var link = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
+      loadContent(link);
+    }
+    everPushedSomething = true;
+  });
 
 
 })();
