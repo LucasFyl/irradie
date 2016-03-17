c6a6017a55b31ccda4fd458f829fa9c7c804b2d4
Lucas Fayolle <lfayolle1@gmail.com>
2016-03-17 16:24:15 +0100

    extend content with appropriate data parallax and size for each project

--- app/site/snippets/projects.php
+++ app/site/snippets/projects.php
@@ -3,7 +3,7 @@
 
     <figure 
       data-parallax="<?php echo $project->dataParallax() ?>"
-      style="top:<?php echo $project->topPosition() ?>rem;left:<?php echo $project->leftPosition() ?>rem;right:<?php echo $project->rightPosition() ?>rem;width:<?php echo $project->elWidth() ?>rem;height:<?php echo $project->elHeight() ?>rem;">
+      style="top:<?php echo $project->topPosition() ?>;left:<?php echo $project->leftPosition() ?>;right:<?php echo $project->rightPosition() ?>;width:<?php echo $project->elWidth() ?>rem;height:<?php echo $project->elHeight() ?>rem;">
       <?php if ($project->hasImages()): ?>
         <a href="<?php echo $project->url() ?>" class="ajaxy">
           <img src="<?php echo $project->image('01-cover.jpg')->url() ?>" alt="<?php echo $project->title(); ?>" />
